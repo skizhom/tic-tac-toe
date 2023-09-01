3ac223df4b22be4142c53ac5061570970557f5a7
bestestest kitteh <236551@edu.p.lodz.pl>
2023-09-01 14:20:30 +0200

    cleanup

--- src/tic_tac_toe/model.clj
+++ src/tic_tac_toe/model.clj
@@ -24,71 +24,31 @@
 
 (defn click [state position])
 
-<<<<<<< HEAD
-
 (defn col-wise
-      [matrix]
-      (apply map vector matrix))
+  [matrix]
+  (apply map vector matrix))
 
 (defn diagonaler
-      [matrix]
-      (map nth matrix [0 1 2]))
+  [matrix]
+  (map nth matrix [0 1 2]))
 
 (defn diagaonaler-inv
-      [matrix]
-      (map nth (reverse matrix) [0 1 2]))
+  [matrix]
+  (map nth (reverse matrix) [0 1 2]))
 
 (defn matrix-permutations
-      [matrix]
-      (apply conj ((juxt col-wise first second last diagonaler diagaonaler-inv) matrix)))
+  [matrix]
+  (apply conj ((juxt col-wise first second last diagonaler diagaonaler-inv) matrix)))
 
 (defn continue?
-      [matrix]
-      (if (some nil? (vec (flatten matrix))) :continue "draw"))
+  [matrix]
+  (if (some nil? (vec (flatten matrix))) :continue "draw"))
 
 (defn matrix-check
-      [matrix]
-      (let [setted-coll (set (matrix-permutations matrix))
-            result        (clojure.set/intersection #{[:x :x :x] [:o :o :o]} setted-coll)]
-               (cond
-                 (= result #{[:x :x :x]}) "x won"
-                 (= result #{[:o :o :o]}) "o won"
-                 :else                    (continue? matrix))))
-
-=======
-
-;(defn size [coll]
-;  [(count coll) (count (first coll))])
-;
-;(defn transpose [x]
-;  (apply mapv vector x))
-;
-;(def reversev (comp vec reverse))
-;
-;(def rotate90deg "shape->shape"
-;  (comp transpose reversev))
-;
-;(defn rotations "shape->shape"
-;  [x]
-;  (->> x
-;       (iterate rotate90deg)
-;       (take 4)))
-;
-;(defn positions "returns a list of sets of points which the rule covers"
-;  ;; keeping "shape" after this would be more and more cumbersome
-;  [shape area]
-;  (let [[a-x a-y] area
-;        [s-x s-y] (size shape)
-;        diff-x (- a-x s-x)
-;        diff-y (- a-y s-y)]
-;    (for [ox (range (inc diff-x))
-;          oy (range (inc diff-y))]
-;      ;; this can be done first (TODO)
-;      (->> (for [x (range s-x)
-;                 y (range s-y)
-;                 :when (get-in shape [x y])]
-;             [(+ ox x) (+ oy y)])
-;           (into #{})))))
-;
-;(defn victor [state])
-;>>>>>>> 271bdbd36d7cb4bd908493397422cbde5671da8e
+  [matrix]
+  (let [setted-coll (set (matrix-permutations matrix))
+        result        (clojure.set/intersection #{[:x :x :x] [:o :o :o]} setted-coll)]
+    (cond
+      (= result #{[:x :x :x]}) "x won"
+      (= result #{[:o :o :o]}) "o won"
+      :else                    (continue? matrix))))
