be4ecc1e4044219887399a4e74df8dd8339688ad
bestestest kitteh <236551@edu.p.lodz.pl>
2023-09-14 19:52:01 +0200

    + coercion interceptor (couldn't find any)

--- project.clj
+++ project.clj
@@ -15,11 +15,12 @@
                  [io.pedestal/pedestal.jetty "0.6.0"]
                  ;; [io.pedestal/pedestal.immutant "0.6.0"]
                  ;; [io.pedestal/pedestal.tomcat "0.6.0"]
-
                  [ch.qos.logback/logback-classic "1.2.10" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.35"]
                  [org.slf4j/jcl-over-slf4j "1.7.35"]
-                 [org.slf4j/log4j-over-slf4j "1.7.35"]]
+                 [org.slf4j/log4j-over-slf4j "1.7.35"]
+                 [prismatic/schema "1.4.1"]
+                 [better-cond "2.1.5"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
--- src/ttt_be/service.clj
+++ src/ttt_be/service.clj
@@ -2,26 +2,41 @@
   (:require [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]
             [io.pedestal.http.body-params :as body-params]
+            [io.pedestal.http.params :as params]
+            [better-cond.core :as b]
+            ;; [io.pedestal.interceptor.helpers :as interceptor]
+            [schema.core :as s]
+            [schema.coerce :as coerce]
+            ;; [io.pedestal.http.ring-middlewares :as middleware]
             [ring.util.response :as ring-resp]))
 
-(defn about-page
+(def unify-body-params
+  {:enter (fn [{:keys [request] :as context}]
+            (assoc-in context [:request :body-params] (or (:edn-params  request) (:json-params request))))})
+(defn echo
   [request]
-  (ring-resp/response (format "Clojure %s - served from %s"
-                              (clojure-version)
-                              (route/url-for ::about-page))))
+  (ring-resp/response (pr-str request)))
 
-(defn home-page
-  [request]
-  (ring-resp/response "Hello World!"))
+(defn verify-body [schema]
+  (let [coercer (coerce/coercer schema coerce/json-coercion-matcher)
+        path [:request :body-params]]
+    {:enter (fn [context]
+              (let [body (get-in context path)
+                    coerced (coercer body)]
+                (if (isa? (type coerced)  schema.utils.ErrorContainer)
+                  (assoc context :response (ring-resp/bad-request (pr-str (:error coerced))))
+                  (assoc-in context path coerced))))}))
 
 ;; Defines "/" and "/about" routes with their associated :get handlers.
 ;; The interceptors defined after the verb map (e.g., {:get home-page}
 ;; apply to / and its children (/about).
-(def common-interceptors [(body-params/body-params) http/html-body])
+(def common-interceptors [(body-params/body-params) unify-body-params])
+
+(defn interceptors [& specific] (vec (concat common-interceptors specific)))
 
 ;; Tabular routes
-(def routes #{["/" :get (conj common-interceptors `home-page)]
-              ["/about" :get (conj common-interceptors `about-page)]})
+(def routes
+  #{["/test" :post (interceptors (verify-body s/Int) `echo)]})
 
 ;; Map-based routes
 ;(def routes `{"/" {:interceptors [(body-params/body-params) http/html-body]
@@ -33,7 +48,6 @@
 ;  `[[["/" {:get home-page}
 ;      ^:interceptors [(body-params/body-params) http/html-body]
 ;      ["/about" {:get about-page}]]]])
-
 
 ;; Consumed by ttt-be.server/create-server
 ;; See http/default-interceptors for additional options you can configure
