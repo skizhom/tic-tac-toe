2147a6a5033b6d0f0bb86896e9ce872d65a7a6a0
bestestest kitteh <236551@edu.p.lodz.pl>
2023-09-18 12:48:04 +0200

    fix tests

--- test/ttt_be/service_test.clj
+++ test/ttt_be/service_test.clj
@@ -22,10 +22,11 @@
 
 (deftest register-player-test
   (let [resp1 (response-edn service :post "/register-player" {:name "test1"})
-        body (-> (response-edn service :post "/register-player" {:name "test1"})
-                 :body
-                 edn/read-string)]
+        body (-> resp1 :body edn/read-string)
+        resp2 (response-edn service :post "/register-player" {:name "test1"})]
     (is (= 200 (:status resp1)))
     (is (= "test1" (:name body)))
     (is (uuid? (:id body)))
-    (is (uuid? (:token body)))))
+    (is (uuid? (:token body)))
+    (is (= 400 (:status resp2)))
+    (is (= 1 (count (:players @s/state))))))
--- test/ttt_be/state_test.clj
+++ test/ttt_be/state_test.clj
@@ -1,7 +1,23 @@
 (ns ttt-be.state-test
-  (:require [ttt-be.state :as sut]
+  (:require [ttt-be.state :as s]
             [malli.core :as m]
-            [clojure.test :refer [deftest is]]))
-
+            [io.pedestal.interceptor.chain :as chain]
+            [clojure.test :refer [deftest is use-fixtures]]
+            [io.pedestal.interceptor :as interceptor]))
+(use-fixtures :each (fn [f]
+                      (reset! s/state s/initial-state)
+                      (f)
+                      (reset! s/state s/initial-state)))
 (deftest initial-state-is-valid
-  (is (m/validate sut/State sut/initial-state)))
+  (is (m/validate s/State s/initial-state)))
+
+(deftest transactions
+  (let [test-chain #(map interceptor/interceptor
+                         [s/state-interceptor
+                          {:name :test
+                           :enter (fn [context]
+                                    (assoc context :tx-data [(constantly %)]))}])]
+    (chain/execute {} (test-chain :val))
+    (is (= :val @s/state))
+    (chain/execute {} (test-chain :val2))
+    (is (= :val2 @s/state))))
